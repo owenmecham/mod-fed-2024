f924059a1fe17a8d655822d90715c3fac5850285
owenmecham <owen.mecham@gmail.com>
2024-03-17 20:04:16 -0600

    feat: added webpack config for mfe2

--- mfe2/src/main.ts
+++ mfe2/src/main.ts
@@ -1,6 +1,2 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import('./bootstrap')
+	.catch(err => console.error(err));
