09079a62ef3cd87008f3415de6627501baf8e36b
owenmecham <owen.mecham@gmail.com>
2024-03-17 20:25:45 -0600

    feat: working dynamic plugins

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { WalletLibService } from 'wallet-lib';
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {
-  balance = 0;
-  constructor(private walletService: WalletLibService) {
-	this.balance = this.walletService.balance;
+import { Component, OnInit } from '@angular/core';
+  import { WalletLibService } from 'wallet-lib';
+  import { LookupService } from '../plugins/lookup.service';
+  import { PluginOptions } from '../plugins/plugin';
+  @Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+  })
+  export class HomeComponent implements OnInit {
+    balance = 0;
+    plugins: PluginOptions[] = [];
+    graphs: PluginOptions[] = [];
+    constructor(private walletService: WalletLibService, private lookupService: LookupService) {
+    this.balance = this.walletService.balance;
+    }
+    async ngOnInit(): Promise<void> {
+    this.plugins = await this.lookupService.lookup();
+    }
+    handleClick(graph: PluginOptions): void {
+    this.graphs.push(graph);
+    }
   }
-}
