967a800df2123cedf5c286547e9af331d936ede8
owenmecham <owen.mecham@gmail.com>
2024-03-17 18:24:59 -0600

    feat: working dynamic federation

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,22 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { loadRemoteModule } from '@angular-architects/module-federation';
+  import { HomeComponent } from './home/home.component';
+  import { NotFoundComponent } from './not-found/not-found.component';
 
-export const APP_ROUTES: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full',
-  },
-
-  {
-    path: 'cryptos',
-    loadChildren: () => loadRemoteModule({ type: 'manifest', remoteName: 'mfe1', exposedModule: './Module'}).then(m => m.CryptosModule)
+  export const APP_ROUTES: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+      pathMatch: 'full',
     },
-
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
-];
+    {
+      path: '**',
+      component: NotFoundComponent,
+    },
+  ];
